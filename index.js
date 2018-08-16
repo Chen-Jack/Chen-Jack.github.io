e0bdabb34bdaf43b86635335c64f2edc31cabf86
Jack C <chen.jack.cs@gmail.com>
2018-08-16 03:47:41 -0400

    added functionality for navbar highlighting

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ window.onload = ()=>{
     const landing = document.getElementById("main");
     landing.classList.add("show-main");
 }
+
+const nav_links = {
+    'about': document.getElementById('about-link'),
+    'portfolio': document.getElementById('portfolio-link'),
+    'education': document.getElementById('education-link')
+};
+
+//Initalize which nav link is highlighted be default
+let current_selection = 'about'; //Default selection
+nav_links[current_selection].classList.add('selected-link')
+
+const updateSelection=(selection)=>{
+    if(current_selection != null){
+        nav_links[current_selection].classList.remove('selected-link')
+    }
+    current_selection = selection;
+    nav_links[current_selection].classList.add('selected-link')
+}
