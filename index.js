ba8a58fc2ecceae05cf970ad5092cbc85e153355
Jack C <chen.jack.cs@gmail.com>
2018-08-16 21:18:29 -0400

    added scrolling wheel functionality

--- index.js
+++ index.js
@@ -10,42 +10,50 @@ window.onload = ()=>{
     window.onwheel = changeSectionOnWheel
 }
 
-const nav_links = {
-    'about': document.getElementById('about-link'),
-    'portfolio': document.getElementById('portfolio-link'),
-    'education': document.getElementById('education-link')
-};
+// const nav_links = {
+//     'about': document.getElementById('about-link'),
+//     'portfolio': document.getElementById('portfolio-link'),
+//     'education': document.getElementById('education-link')
+// };
 
-
-
-const nav_links_arr = [];
-for(let i in nav_links){
-    nav_links_arr.push(nav_links[i])
-}
-
-function changeSectionOnWheel(wheelEvent){
-    let dy = wheelEvent.deltaY;
-    if(dy > 0){
-        console.log("postive");
-    }
-    else if(dy < 0){
-        console.log("neg");
-    }
-    else{
-        //do nothing
-    }
+const nav_links = document.getElementById('nav-bar').children
+for(let i = 0; i<nav_links.length; i++){
+    nav_links[i].onclick = updateCurrentSelection.bind(this, i)
 }
 
 //Initalize which nav link is highlighted be default
-let current_selection = 'about'; //Default selection
+let current_selection = 0; //Default selection is the first link
 nav_links[current_selection].classList.add('selected-link')
-
-const updateSelection=(selection)=>{
+function updateCurrentSelection(selection){
+    console.log("you selected", selection)
     if(current_selection != null){
         nav_links[current_selection].classList.remove('selected-link')
     }
     current_selection = selection;
     nav_links[current_selection].classList.add('selected-link')
 }
+
+
+function changeSectionOnWheel(wheelEvent){
+    let dy = wheelEvent.deltaY;
+    
+    if(dy > 0 && current_selection != nav_links.length-1){ //Prevent wrap
+        current_selection +=1;
+        nav_links[current_selection].click();
+        updateCurrentSelection(current_selection);
+    }
+    
+    else if(dy < 0 && current_selection != 0){  //Prevent wrap
+        current_selection -=1;
+        nav_links[current_selection].click();
+        updateCurrentSelection(current_selection);
+    }
+
+    //To prevent constant calling of this function
+    window.onwheel = null;
+    setTimeout(()=>window.onwheel = changeSectionOnWheel, 200);
+}
+
+
 
 
